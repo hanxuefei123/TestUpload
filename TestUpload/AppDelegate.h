03e82025ba29f40f6f758910a142403fc6a0cd0a
hxf <513596992@qq.com>
2018-08-09 10:37:25 +0800

    Initial Commit

---
+++ TestUpload/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestUpload
+//
+//  Created by RuiFeng on 2018/8/9.
+//  Copyright © 2018年 RuiFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestUpload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestUpload
+//
+//  Created by RuiFeng on 2018/8/9.
+//  Copyright © 2018年 RuiFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
